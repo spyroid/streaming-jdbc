45e2d78d1b871a86d90cc993c010d9fc14ef65d2
spyroid <soyroid@gmail.com>
2024-07-22 15:10:30 +0200

    init

---
+++ src/main/java/symetricum/jdbc/StreamingJdbcApplication.java
@@ -0,0 +1,13 @@
+package symetricum.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/symetricum/jdbc/StreamingJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package symetricum.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
